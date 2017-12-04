fd122dea2c852f6677ccd56bc5aba483aeee341e
ekansrm <ekansrm@gmail.com>
2017-12-04 08:50:32 +0800

    RESTful and WebSocket

--- src/main/java/com/ekansrm/mlas/controller/root/RootController.java
+++ src/main/java/com/ekansrm/mlas/controller/root/RootController.java
@@ -1,5 +1,8 @@
 package com.ekansrm.mlas.controller.root;
 
+import com.ekansrm.mlas.controller.root.dto.response.Response;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
+  static private Gson gson;
+  static {
+    gson = new GsonBuilder().setPrettyPrinting().create();
+  }
+
   @RequestMapping(
       value = "/",
+      method = RequestMethod.GET
+  )
+  public String root() {
+    return "<h1>Machine Learning as Service</h1>";
+  }
+
+  @RequestMapping(
+      value = "/about",
       method = RequestMethod.GET,
       produces = "application/json;charset=UTF-8"
   )
-  public String root() {
-    return "Machine Learning as Service";
+  public String about() {
+    return gson.toJson(Response.successResponse("MLasS 是实现将机器学习模型部署到线上的一个脚手架"));
   }
 
 }
---
+++ src/main/java/com/ekansrm/mlas/controller/root/dto/message/Message.java
@@ -0,0 +1,9 @@
+package com.ekansrm.mlas.controller.root.dto.message;
+
+import lombok.Data;
+
+@Data
+public class Message {
+
+  private String name;
+}
