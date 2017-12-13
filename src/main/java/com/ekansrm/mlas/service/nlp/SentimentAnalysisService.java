23a792d8f2a1fff1de7e3bb70263bb1c059b0d44
ekansrm <ekansrm@gmail.com>
2017-12-13 08:41:24 +0800

    情感分析Service 和 Controller, 与 xmlrpc 连接

--- src/main/java/com/ekansrm/mlas/service/nlp/SentimentAnalysisService.java
+++ src/main/java/com/ekansrm/mlas/service/nlp/SentimentAnalysisService.java
@@ -1,4 +1,9 @@
 package com.ekansrm.mlas.service.nlp;
 
+import org.apache.xmlrpc.XmlRpcException;
+
 public interface SentimentAnalysisService {
+
+  String predict(String text) throws XmlRpcException;
+
 }
